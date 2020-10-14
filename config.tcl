083f1b8703c677e5110911e0b8423933a172bbbf
Matt Venn <matt@mattvenn.net>
2020-10-14 10:09:53 +0200

    use same config as clockless

--- config.tcl
+++ config.tcl
@@ -9,16 +9,10 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 set ::env(CLOCK_PERIOD) "10.000"
 set ::env(CLOCK_PORT) "clk"
 
-set ::env(PL_TARGET_DENSITY) 0.9
-set ::env(FP_CORE_UTIL) 10; # 10 % utilization
-set ::env(FP_PDN_VOFFSET) 15
-set ::env(FP_PDN_VPITCH) 5
-set ::env(FP_PDN_HOFFSET) 15 
-set ::env(FP_PDN_HPITCH) 5
+set ::env(FP_CORE_UTIL) 5
+set ::env(PL_TARGET_DENSITY) 0.5
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
 }
-
-
